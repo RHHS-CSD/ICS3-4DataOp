b3ae6c4897c0222137cc3e042bb6ff3ec06cda06
michael.roy-diclemen <michael.roy-diclemen@TEACH2D-C3NYBS3.EDU.YRDSB.NET>
2024-09-24 14:28:07 -0400

    Initial Commit

---
+++ src/ics3uoperations/ICS3UOperations.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ics3uoperations;
+
+/**
+ *
+ * @author michael.roy-diclemen
+ */
+public class ICS3UOperations {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Question 1 - minutes
+        
+        //Question 2 - Temperature
+        
+        //Question 3 - digitizer
+    }
+    
+}
